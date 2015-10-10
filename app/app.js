8d7f7c427c10667aaf7977664f985eebfd06d7b2
peter <peter.k.nets@gmail.com>
2015-10-10 18:38:45 +0300

    Add first NG-DIRECTIVE

--- app/app.js
+++ app/app.js
@@ -1,12 +1,20 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+angular.module('listApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
   'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+  ])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider.otherwise({redirectTo: '/view1'});
+  }])
+  .controller('ListController', function() {
+  })
+  .directive('mainHeader', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'partials/main-header.html'
+    };
+  });
