386fec50261173b83b473198ebedf06240d1631f
peter <peter.k.nets@gmail.com>
2015-10-11 10:55:40 +0300

    Add 'Add to Cart' button and use NG-SHOW

--- app/app.js
+++ app/app.js
@@ -4,23 +4,28 @@
       this.listItems = [
         {
           url: "img/numbers-01.jpg",
-          author: "Provided by: &copy; Tom Sawyer, 1889."
+          author: "Provided by: &copy; Tom Sawyer, 1889.",
+          canPurchase: false
         },
         {
           url: "img/numbers-02.jpg",
-          author: "Provided by: &copy; Jimi Hendrix, 1969."
+          author: "Provided by: &copy; Jimi Hendrix, 1969.",
+          canPurchase: true
         },
         {
           url: "img/numbers-06.jpg",
-          author: "Provided by: &copy; Freddy Mercury, 1989."
+          author: "Provided by: &copy; Freddy Mercury, 1989.",
+          canPurchase: true
         },
         {
           url: "img/numbers-09.jpg",
-          author: "Provided by: &copy; Kurt Cobain, 1991."
+          author: "Provided by: &copy; Kurt Cobain, 1991.",
+          canPurchase: true
         },
         {
           url: "img/numbers-12.jpg",
-          author: "Provided by: &copy; Jim Morrison, 1970."
+          author: "Provided by: &copy; Jim Morrison, 1970.",
+          canPurchase: true
         }
       ];
     })
