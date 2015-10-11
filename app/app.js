4fb3724c3bd702b19f76db6d3cb84d0b841d0cf5
peter <peter.k.nets@gmail.com>
2015-10-11 11:21:20 +0300

    Add filters

--- app/app.js
+++ app/app.js
@@ -5,30 +5,35 @@
         {
           url: "img/numbers-01.jpg",
           author: "Provided by: &copy; Tom Sawyer, 1889.",
+          price: 20.05,
           canPurchase: false,
           soldOut: false
         },
         {
           url: "img/numbers-02.jpg",
           author: "Provided by: &copy; Jimi Hendrix, 1969.",
+          price: 71.20,
           canPurchase: true,
           soldOut: false
         },
         {
           url: "img/numbers-06.jpg",
           author: "Provided by: &copy; Freddy Mercury, 1989.",
+          price: 35.00,
           canPurchase: true,
           soldOut: false
         },
         {
           url: "img/numbers-09.jpg",
           author: "Provided by: &copy; Kurt Cobain, 1991.",
+          price: 99.99,
           canPurchase: true,
           soldOut: false
         },
         {
           url: "img/numbers-12.jpg",
           author: "Provided by: &copy; Jim Morrison, 1970.",
+          price: 114.21,
           canPurchase: true,
           soldOut: false
         }
