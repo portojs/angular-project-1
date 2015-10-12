71c3dc9e0497e06e3470b46a18dd168885a3d8fd
peter <peter.k.nets@gmail.com>
2015-10-12 12:15:52 +0300

    Add reviews

--- app/app.js
+++ app/app.js
@@ -6,6 +6,12 @@
           url: "img/numbers-01.jpg",
           author: "Provided by: &copy; Tom Sawyer, 1889.",
           description: "A unique piece of art made by one of the brightest minds in human history.",
+          reviews: [
+            {
+              text: "Fabulous!",
+              author: "dean@gmail.com"
+            }
+          ],
           price: 20.05,
           canPurchase: false,
           soldOut: false
@@ -14,6 +20,12 @@
           url: "img/numbers-02.jpg",
           author: "Provided by: &copy; Jimi Hendrix, 1969.",
           description: "A unique piece of art made by one of the brightest minds in human history.",
+          reviews: [
+            {
+              text: "Fantastic!",
+              author: "dean@gmail.com"
+            }
+          ],
           price: 71.20,
           canPurchase: true,
           soldOut: false
@@ -22,6 +34,12 @@
           url: "img/numbers-06.jpg",
           author: "Provided by: &copy; Freddy Mercury, 1989.",
           description: "A unique piece of art made by one of the brightest minds in human history.",
+          reviews: [
+            {
+              text: "Formidable!",
+              author: "dean@gmail.com"
+            }
+          ],
           price: 35.00,
           canPurchase: true,
           soldOut: false
@@ -30,6 +48,12 @@
           url: "img/numbers-09.jpg",
           author: "Provided by: &copy; Kurt Cobain, 1991.",
           description: "A unique piece of art made by one of the brightest minds in human history.",
+          reviews: [
+            {
+              text: "Ferocious!",
+              author: "dean@gmail.com"
+            }
+          ],
           price: 99.99,
           canPurchase: true,
           soldOut: false
@@ -38,6 +62,12 @@
           url: "img/numbers-12.jpg",
           author: "Provided by: &copy; Jim Morrison, 1970.",
           description: "A unique piece of art made by one of the brightest minds in human history.",
+          reviews: [
+            {
+              text: "Fearsome!",
+              author: "dean@gmail.com"
+            }
+          ],
           price: 114.21,
           canPurchase: true,
           soldOut: false
