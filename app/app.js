d4eb66a0978e278053f718dec8210be8083916d2
peter <peter.k.nets@gmail.com>
2015-10-14 17:53:50 +0300

    Create 'product' module

--- app/app.js
+++ app/app.js
@@ -94,41 +94,5 @@
         },
         controllerAs: 'navCtrl'
       };
-    })
-    .directive('productInfo', function() {
-      return {
-        restrict: 'E',
-        templateUrl: 'partials/product-info.html'
-      }
-    })
-    .directive('productForm', function() {
-      return {
-        restrict: 'E',
-        templateUrl: 'partials/product-form.html',
-        controller: function() {
-          this.review = {};
-          this.addReview = function(item) {
-            item.reviews.push(this.review);
-            this.review = {};
-          };
-        },
-        controllerAs: 'reviewCtrl'
-      };
-    })
-    .directive('productPanels', function() {
-      return {
-        restrict: 'E',
-        templateUrl: 'partials/product-panels.html',
-        controller: function(){
-          this.tab = 1;
-          this.selectTab = function(setTab) {
-            this.tab = setTab;
-          };
-          this.isSelected = function(checkTab) {
-            return checkTab === this.tab;
-          };
-        },
-        controllerAs: 'panelCtrl'
-      };
     });
 })();
---
+++ app/products.js
@@ -0,0 +1,39 @@
+(function(){
+  angular.module('list-products', [])
+    .directive('productInfo', function() {
+      return {
+        restrict: 'E',
+        templateUrl: 'partials/product-info.html'
+      }
+    })
+    .directive('productForm', function() {
+      return {
+        restrict: 'E',
+        templateUrl: 'partials/product-form.html',
+        controller: function() {
+          this.review = {};
+          this.addReview = function(item) {
+            item.reviews.push(this.review);
+            this.review = {};
+          };
+        },
+        controllerAs: 'reviewCtrl'
+      };
+    })
+    .directive('productPanels', function() {
+      return {
+        restrict: 'E',
+        templateUrl: 'partials/product-panels.html',
+        controller: function(){
+          this.tab = 1;
+          this.selectTab = function(setTab) {
+            this.tab = setTab;
+          };
+          this.isSelected = function(checkTab) {
+            return checkTab === this.tab;
+          };
+        },
+        controllerAs: 'panelCtrl'
+      };
+    });
+})();
