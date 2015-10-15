750cc188fe221c46be0f14183631f0ad9843297d
peter <peter.k.nets@gmail.com>
2015-10-15 16:34:36 +0300

    TEST - it seems json file is not loading anymore, usual variable is loading

--- app/app.js
+++ app/app.js
@@ -2,14 +2,14 @@
   angular.module('listApp', ['list-products'])
     .controller('ListController', ['$http', function($http) {
       var list = this;
-      list.listItems = [];
-      list.itemTypes = [];
-      $http.get('products.json').success(function(data) {
-        list.listItems = data;
-      });
-      $http.get('products-types.json').success(function(data) {
-        list.itemTypes = data;
-      });
+      list.listItems = doom;
+      //list.itemTypes = [];
+      //$http.get('products.json').success(function(data) {
+      //  list.listItems = data;
+      //});
+      //$http.get('products-types.json').success(function(data) {
+      //  list.itemTypes = data;
+      //});
     }])
     .directive('mainHeader', function() {
       return {
@@ -27,4 +27,22 @@
         controllerAs: 'navCtrl'
       };
     });
+  var doom = [
+    {
+      url: "img/numbers-01.jpg",
+      author: "Provided by: &copy; Tom Sawyer, 1889.",
+      description: "A unique piece of art made by one of the brightest minds in human history.",
+      type: {"borrowed": true, "graphics": true},
+      reviews: [
+        {
+          stars: 5,
+          text: "Fabulous!",
+          author: "dean@gmail.com"
+        }
+      ],
+      price: 20.05,
+      canPurchase: false,
+      soldOut: false
+    }
+  ];
 })();
